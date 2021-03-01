5831d4989a27c9ec0f4e8e1e650e48a54ce75267
noh jiseong <no0683@naver.com>
2021-03-01 22:17:32 +0900

    포트폴리오 만들기(ver.21/03/01 22:17)

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -1,13 +1,17 @@
 var header_left_btn = document.querySelector("#header_left_btn");
 var header_left_nav = document.querySelector("#header_left_nav");
+var header_left_ham = document.querySelector("#header_left_btn > a > span");
+var header_left_a = document.querySelector("#header_left_btn > a");
 
 
 header_left_btn.onclick = function(){
     if(header_left_nav.classList != "live"){
-        header_left_nav.style.opacity = 1;
         header_left_nav.classList = "live";
+        header_left_ham.classList = "live";
+        header_left_btn.classList = "live";
     } else {
-        header_left_nav.style.opacity = 0;
         header_left_nav.classList = "";
+        header_left_ham.classList = "";
+        header_left_btn.classList = "";
     }
 };
