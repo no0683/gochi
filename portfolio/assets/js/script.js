5952de301cc062c82b80486ea7dd6a27241a3e33
noh jiseong <no0683@naver.com>
2021-03-03 19:14:14 +0900

    포트폴리오 만들기(ver.21/03/03 19:14)

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -1,3 +1,4 @@
+// 헤더 버튼 효과
 var header_left_btn = document.querySelector("#header_left_btn");
 var header_left_nav = document.querySelector("#header_left_nav");
 var header_left_ham = document.querySelector("#header_left_btn > a > span");
@@ -15,3 +16,62 @@ header_left_btn.onclick = function(){
         header_left_btn.classList = "";
     }
 };
+
+// 패럭랙스 효과
+window.addEventListener("scroll", function(){
+    var pageYOffset = window.pageYOffset + screen.height / 2;
+
+    // 출력
+    document.querySelector(".scrollTop").textContent = parseInt(pageYOffset);
+    document.querySelector(".s10T").textContent = document.querySelector("#intro").offsetTop;
+    document.querySelector(".s20T").textContent = document.querySelector("#skills").offsetTop;
+    document.querySelector(".s30T").textContent = document.querySelector("#result1").offsetTop;
+    document.querySelector(".s40T").textContent = document.querySelector("#result2").offsetTop;
+    document.querySelector(".s50T").textContent = document.querySelector("#result3").offsetTop;
+    document.querySelector(".s60T").textContent = document.querySelector("#the_others").offsetTop;
+    document.querySelector(".s70T").textContent = document.querySelector("#contacts").offsetTop;
+
+    // 출력(레드)
+    if(pageYOffset > document.querySelector("#intro").offsetTop){
+        document.querySelector(".s10T").style.color = "red";
+    } else {
+        document.querySelector(".s10T").style.color = "black";
+    }
+    if(pageYOffset > document.querySelector("#skills").offsetTop){
+        document.querySelector(".s20T").style.color = "red";
+    } else {
+        document.querySelector(".s20T").style.color = "black";
+    }
+    if(pageYOffset > document.querySelector("#result1").offsetTop){
+        document.querySelector(".s30T").style.color = "red";
+    } else {
+        document.querySelector(".s30T").style.color = "black";
+    }
+    if(pageYOffset > document.querySelector("#result2").offsetTop){
+        document.querySelector(".s40T").style.color = "red";
+    } else {
+        document.querySelector(".s40T").style.color = "black";
+    }
+    if(pageYOffset > document.querySelector("#result3").offsetTop){
+        document.querySelector(".s50T").style.color = "red";
+    } else {
+        document.querySelector(".s50T").style.color = "black";
+    }
+    if(pageYOffset > document.querySelector("#the_others").offsetTop){
+        document.querySelector(".s60T").style.color = "red";
+    } else {
+        document.querySelector(".s60T").style.color = "black";
+    }
+    if(pageYOffset > document.querySelector("#contacts").offsetTop){
+        document.querySelector(".s70T").style.color = "red";
+    } else {
+        document.querySelector(".s70T").style.color = "black";
+    }
+
+    // 나타내기
+    if(pageYOffset > document.querySelector("#result3").offsetTop){
+        document.querySelector("#mega").classList.add("show");
+    } else {
+        document.querySelector("#mega").classList.remove("show");
+    }
+});
