a1f5051b416491694cbae28b63170d8e64235271
JISEONG NOH <no0683@naver.com>
2021-04-21 22:13:25 +0900

    리액트 입문-redux 적용-환경설정(ver.21/04/21 22:12)

---
+++ react/리액트 와 Redux/3일차(redux 적용-환경설정)/src/App.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./styles.css";
+import AddNumberRoot from "./Components/AddNumberRoot";
+import DisplayNumberRoot from "./Components/DisplayNumberRoot";
+
+// 상위 컴포넌트에서 하위 컴포넌트로 값을 보낼때는 props로 연결
+// 하위 컴포넌트에서 상위 컴포넌트로 값을 보낼때는 function으로 연결
+// Redux도입하지않고 만약 부모,자식간의 컴포넌트 depth가 많다면.. 컴포넌트간의 연결관계가 복잡해진다
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot></DisplayNumberRoot>
+      </div>
+    );
+  }
+}
---
+++ react/리액트 와 Redux/3일차(redux 적용-환경설정)/src/Components/DisplayNumber.jsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import store from "../store";
+
+export default class DisplayNumber extends Component {
+  state = { number: store.getState().number };
+  constructor(props) {
+    super(props);
+    store.subscribe(
+      function () {
+        this.setState({ number: store.getState().number });
+      }.bind(this)
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h1>DisplayNumber</h1>
+        <input type="text" value={this.state.number} readOnly></input>
+      </div>
+    );
+  }
+}
