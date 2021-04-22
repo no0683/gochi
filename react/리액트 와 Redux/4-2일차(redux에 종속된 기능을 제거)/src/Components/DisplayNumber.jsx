5f1604efc4ef82d42693091886a91bcd131c993c
JISEONG NOH <no0683@naver.com>
2021-04-22 19:38:57 +0900

    React와Redux-redux에 종속된 기능을 제거(ver.21/04/22 19:38)

---
+++ react/리액트 와 Redux/4-2일차(redux에 종속된 기능을 제거)/src/Components/DisplayNumber.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class DisplayNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DisplayNumber</h1>
+        <input type="text" value={this.props.number} readOnly></input>
+      </div>
+    );
+  }
+}
